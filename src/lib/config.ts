f066cd752185edae0219ee0cf117bcd0ff32d72f
Sarthak <sarthakdubey707@gmail.com>
2025-10-03 18:09:40 +0000

    give the admin role arpit.jio6565@gmail.com

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,5 +3,5 @@
 // IMPORTANT: Replace this with the actual Firebase UID of your admin users.
 // You can find a user's UID in the Firebase Console under Authentication > Users.
 export const ADMIN_USER_IDS = [
-  'REPLACE_WITH_YOUR_ADMIN_USER_ID',
+  'zgCBWGAWP3TvCkjdABmixpJCI2u2',
 ];
